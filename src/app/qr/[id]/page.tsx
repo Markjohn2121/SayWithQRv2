39481af7c7db8660c023a6a98b792b7c6a1c7e87
Markjohn Valdez <86964778+Markjohn2121@users.noreply.github.com>
2025-08-16 11:28:51 +0800

    Update page.tsx

---
+++ src/app/qr/[id]/page.tsx
@@ -0,0 +1,9 @@
+import QrArtStudio from '@/components/qr-art-studio';
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4 md:p-8">
+      <QrArtStudio />
+    </main>
+  );
+}
