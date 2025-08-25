62263098ad5a8debb96aa0b332fffa55045baa2b
Markjohn2121 <valdezmarkjohn21@gmail.com>
2025-08-25 20:16:26 +0800

    update qr-art-studio.tsx

--- src/components/qr-art-studio.tsx
+++ src/components/qr-art-studio.tsx
@@ -608,7 +608,7 @@ export default function QrArtStudio() {
       eyeRadius: 8,
       padding: 16,
       canvasShape: 'square',
-      text: "<text>@SayWith</text>",
+      text: "<text>@SayWithit</text>",
       useImage: isImageDesign,
       imageFilter: 'none',
       imageBlur: 0,
