09d56a54efaf0e6e8b66a12b6d902d977ae72ee1
Markjohn2121 <valdezmarkjohn21@gmail.com>
2025-08-30 14:12:23 +0800

    update qr-art-studio-v2.tsx

--- src/components/qr-art-studio-v2.tsx
+++ src/components/qr-art-studio-v2.tsx
@@ -529,6 +529,7 @@ export default function QrArtStudioV2({ qrId, id }: { qrId?: string, id?: string
   
   const replacePathsInTargetG = (svgContent, design) => {
     // Helper: evaluate ${...} inside design.text with access to design
+   let qrTexr = "sam";
     const evaluateTemplate = (tpl, context) => {
         return tpl.replace(/\$\{([^}]+)\}/g, (_, code) => {
             try {
